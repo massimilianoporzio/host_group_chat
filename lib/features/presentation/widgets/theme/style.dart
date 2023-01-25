af8994636ba509233feae5291853bb4fb5928bff
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-01-25 10:25:05 +0100

    colors

---
+++ lib/features/presentation/widgets/theme/style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final Color darkPrimaryColor = Color.fromRGBO(49, 155, 60, 1);
+final Color lightPrimaryColor = Color.fromRGBO(200, 230, 201, 1);
+final Color primaryColor = Color.fromRGBO(76, 175, 80, 1);
+final Color colorD96F43 = Color(0xFFD96F43);
+final Color textIconColor = Color.fromRGBO(225, 255, 255, 1);
+final Color textPrimaryColor = Color.fromRGBO(33, 33, 33, 1);
+final Color dividerColor = Color.fromRGBO(189, 189, 189, 1);
+final Color textIconColorGray = Colors.black.withOpacity(0.9);
+final Color greenColor = Color.fromRGBO(76, 175, 80, 1);
+final Color color747480 = Color(0xFF747480);
+final Color colorC1C1C1 = Color(0xFFC1C1C1);
