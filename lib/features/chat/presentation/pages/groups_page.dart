d51d115c723abb5eb19a7757061554ee80651109
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-01-27 23:20:06 +0100

    singleItemGroup

--- lib/features/chat/presentation/pages/groups_page.dart
+++ lib/features/chat/presentation/pages/groups_page.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:host_group_chat/core/constants.dart';
+import 'package:host_group_chat/features/chat/presentation/widgets/single_item_group_widget.dart';
 
 class GroupsPage extends StatelessWidget {
   const GroupsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Groups Page",
-        style: AppConst.defaultTextStyle,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return SingleItemGroupWidget(
+                  onTap: () {},
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/features/chat/presentation/widgets/single_item_group_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class SingleItemGroupWidget extends StatelessWidget {
+  final VoidCallback? onTap;
+
+  const SingleItemGroupWidget({super.key, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 55,
+                  width: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Text(""),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Group name",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        "data",
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.7), fontSize: 14),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 12),
+                        child: Divider(
+                          thickness: 1.5,
+                          color: Colors.white.withOpacity(.5),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
